e230bdcb9b8b8bbe12eba7d2637f9860ab5e960f
jhondeiberd <jhondeiberd@gmail.com>
2022-01-07 20:32:18 -0500

    Day2

---
+++ web_server_1/server.js
@@ -0,0 +1,11 @@
+// const { read } = require('fs')
+// let http = require ('http')
+
+// http.createServer(function (req, res) {
+//     res.writeHead(200, {'content-Type': 'text/html'})
+//     res.write('<h1>Hello word</h1>')
+//     res.end()
+// }).listen(8000)
+
+// console.log('server listening on port 8000')
+
